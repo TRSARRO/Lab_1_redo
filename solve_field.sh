ca3802d0167405d6fe7d4c5fcfee7cb3b66ec3a9
TRSARRO <31739960+TRSARRO@users.noreply.github.com>
2018-04-29 15:54:23 -0400

    Update solve_field.sh

--- solve_field.sh
+++ solve_field.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -u
 
-for file in $(ls -1 Lab_1_HAT-P-27b_processed_images.*.FIT)
+for file in $(ls -1 Lab_1_HAT-P-27b_processed_images.*.fits)
 do 
     solve-field --ra 222.7667 --dec 5.9472 --radius 1 ${file}
 done
