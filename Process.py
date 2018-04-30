ee51adaeb9f96ea0ed27ea7b5992cf56689100a2
TRSARRO <31739960+TRSARRO@users.noreply.github.com>
2018-04-30 03:47:01 -0400

    Update Process.py

--- Process.py
+++ Process.py
@@ -2,10 +2,10 @@
 
 for file in $(ls -1 Lab_1_HAT-P-27b_science_exposures.00000***.fits)
 do
-    ftpixcalc $(basename " $file" .fits)"_dark.fits" a-b a=$file b=Master_Dark.fits
+    ftpixcalc $(basename " $file" .fits)"_dark.fits" a-b a=$file b=master_dark_test_1.fits
 done
 
 for file in $(ls -1 Lab_1_HAT-P-27b_science_exposures.00000***.fits)
 do
-    ftpixcalc $(basename " $file" _dark.fits)"_final.fits" a/b a=$file b=Master_Flat.fits
+    ftpixcalc $(basename " $file" _dark.fits)"_final.fits" a/b a=$file b=master_flat_test_5.fits
 done
